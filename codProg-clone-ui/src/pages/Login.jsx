2ecee041a3fa0eb07ca881535c83b1ff55353e5c
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-15 23:17:04 +0530

    Adding Form component from react-router-dom

--- codProg-clone-ui/src/pages/Login.jsx
+++ codProg-clone-ui/src/pages/Login.jsx
@@ -1,6 +1,31 @@
+import { Form } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Form method="POST">
+      <div>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="email"
+          autoComplete="off"
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          autoCapitalize="off"
+        />
+      </div>
+      <div>
+        <input type="submit" value="login" />
+      </div>
+    </Form>
+  );
 }
 
 export default Login;
